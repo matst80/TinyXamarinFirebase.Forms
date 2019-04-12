dfb313081fe2af547770ddabb6968c0642f32a71
Mats Törnberg <mats.tornberg@gmail.com>
2019-04-12 10:26:01 +0200

    minor storage change ios

--- src/TinyXamarinFirebase.Forms.iOS/FirebaseStorage.cs
+++ src/TinyXamarinFirebase.Forms.iOS/FirebaseStorage.cs
@@ -39,14 +39,22 @@ namespace TinyXamarinFirebase.Froms.iOS
         public void PutFile(string path, string uri, FirebasePromise<FirebaseFileResult> onCompleted)
         {
             var uploadReference = storage.GetRootReference().GetChild(path);
-            uploadReference.PutFile(NSUrl.FromString(uri), null, async (metadata, error) =>
+            uploadReference.PutFile(NSUrl.FromFilename(uri), null, async (metadata, error) =>
             {
-                var downloadUrl = await uploadReference.GetDownloadUrlAsync();
-                onCompleted?.OnComplete(new FirebaseFileResult()
+                try
                 {
-                    IsSuccess = error == null,
-                    DownloadUrl = downloadUrl.AbsoluteString
-                });
+                    var downloadUrl = await uploadReference.GetDownloadUrlAsync();
+                    onCompleted?.OnComplete(new FirebaseFileResult()
+                    {
+                        IsSuccess = error == null,
+                        DownloadUrl = downloadUrl.AbsoluteString
+                    });
+                }
+                catch(Exception ex)
+                {
+                    onCompleted?.OnError(new FirebaseException(ex));
+                }
+
             });
         }
     }
