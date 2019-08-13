7619f62fb87cf7604f73bc06f48e7738b97ac08c
Mats Törnberg <mats_t80@hotmail.com>
2019-08-13 11:11:19 +0200

    minor fixes and forgotten commit

--- src/TinyXamarinFirebase.Forms.Android/DataConverter.cs
+++ src/TinyXamarinFirebase.Forms.Android/DataConverter.cs
@@ -94,7 +94,7 @@ namespace TinyXamarinFirebase.Forms.Droid
                         var objData = prp.GetValue(data);
                         if (objData != null)
                         {
-                            ret.Add(key, ToNative(objData.GetType(), objData));
+                            ret.Add(key, ToNative(prp.PropertyType, objData));
                         }
                         else
                         {
